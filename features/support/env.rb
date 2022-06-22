a60ac54154da979cfb367133762f3be18d4b948d
Vivian Lima <vivian.lima@caminoeducation.com>
2022-06-21 21:37:31 -0300

    add environments

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,8 +4,14 @@ require 'selenium-webdriver'
 require 'site_prism'
 require 'pry'
 
+ENVIRONMENT = env['ENVIRONMENT']
+puts "Environment is runner in >>> #{ENVIRONMENT}"
+
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "environments/#{ENVIRONMENT}.yml")
+
+
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
-    config.app_host = "http://www.google.com.br"
+    config.app_host = CONFIG['url_default']
     config.default_max_wait_time = 10
 end
