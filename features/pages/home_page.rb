03562ea2df8c4257963cec6be6920f916e334300
Vivian Lima <vivian.lima@caminoeducation.com>
2022-08-06 15:56:00 -0300

    include fails scenarios

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -2,16 +2,6 @@ class  Home < SitePrism::Page
     include RSpec::Matchers
     include Capybara::DSL
 
-    element :title_Aprenda_Na_Pratica, 'div[class="title"]'
-    element :btnSobreNos, 'a[href="/#sobre-nos"]'
-    element :btnDepoimentos, 'a[href="/#depoimentos"]'
-    element :btnParceiros, 'a[href="/#parceiros"]'
-    element :btnFaleConosco, 'a[href="/#faleConosco"]'
-    element :title_Sobre_Nos, '#sobre-nos > div.MuiBox-root.css-1ln06le > h3'
-    element :title_depoimentos, '#depoimentos'
-    element :title_parceiros, '#parceiros > h3'
-    element :title_fale_conosco, '#faleConosco > div > h3'
-
     set_url '/'
 
     def validate_text_homePage(titleHomePage)
@@ -27,23 +17,34 @@ class  Home < SitePrism::Page
     def validate_text_menu_sobreNos(sobre_nos)
     
         title_Sobre_Nos = find('#sobre-nos > div.MuiBox-root.css-1ln06le > h3')
+        if title_Sobre_Nos.text.eql?(sobre_nos) != true
+            raise "Expect element: #{sobre_nos}, but returned: #{title_Sobre_Nos.text}"
+        end
     end
 
     
 
     def validate_text_menu_dep(depoimentos)
         title_depoimentos = find('#depoimentos > div.title.MuiBox-root.css-0 > div > h3')
-        
+        if title_depoimentos.text.eql?(depoimentos) != true
+            raise "Expect element: #{depoimentos}, but returned: #{title_depoimentos.text}"
+        end
     end
 
 
     def validate_text_menu_parc(parceiros)
         title_parceiros = find('#parceiros > h3')
+        if title_parceiros.text.eql?(parceiros) != true
+            raise "Expect element: #{parceiros}, but returned: #{title_parceiros.text}"
+        end
     end
 
 
     def validate_text_menu_fale(fale_conosco)
         title_fale_conosco = find('#faleConosco > div > h3')
+        if title_fale_conosco.text.eql?(fale_conosco) != true
+            raise "Expect element: #{fale_conosco}, but returned: #{title_fale_conosco.text}"
+        end
     end
 
 end
--- features/specs/falhasHome.feature
+++ features/specs/falhasHome.feature
@@ -1,34 +1,34 @@
 #language: pt
-
-Funcionalidade: Home Page
+@Falhar
+Funcionalidade: falhar Home Page
 
 Cenario: Acessar home page
 
 Dado que eu acesse a home page do projeto Qa.Coders
-Então devo visualizar a informação "Aprenda na prática."
+Então devo visualizar a informação "Aprenda Legal."
 
 Cenario: Validar Menu Sobre Nós 
 
 Dado que eu acesse a home page do projeto Qa.Coders
 Quando clico no menu "Sobre nós"
-Então devo obter acesso ao título "Sobre nós"
+Então devo obter acesso ao título "Sobre a gente"
 
 Cenario: Validar Menu Depoimentos
 
 Dado que eu acesse a home page do projeto Qa.Coders
 Quando clico em "Depoimentos"
-Então devo enxergar o texto "O Qa.Coders é feito para os alunos"
+Então devo enxergar o texto "O Qa.Coders é feito para a galera"
 
 Cenario: Validar Menu Parceiros
 
 Dado que eu acesse a home page do projeto Qa.Coders
 Quando clico no submenu "Parceiros"
-Então devo ter acesso ao campo "Parceiros"
+Então devo ter acesso ao campo "Parceiross"
 
 Cenario: Validar Menu Fale Conosco
 
 Dado que eu acesse a home page do projeto Qa.Coders
 Quando clico no link "Fale Conosco"
-Então devo acessar a seção "Fale Conosco"
+Então devo acessar a seção "Fale Conosco!"
 
 
